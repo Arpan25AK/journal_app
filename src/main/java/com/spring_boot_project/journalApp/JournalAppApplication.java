7c470079936fe2d78baf3edf158c3ad2bbc3d869
ArpanAk <akarpan2005@gmail.com>
2026-01-22 21:27:42 +0530

    added the email service classs using the javamailfinder

--- src/main/java/com/spring_boot_project/journalApp/JournalAppApplication.java
+++ src/main/java/com/spring_boot_project/journalApp/JournalAppApplication.java
@@ -5,12 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.MongoTransactionManager;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableScheduling
 @EnableTransactionManagement
 public class JournalAppApplication {
 
--- src/main/java/com/spring_boot_project/journalApp/service/EmailService.java
+++ src/main/java/com/spring_boot_project/journalApp/service/EmailService.java
@@ -1,4 +1,28 @@
 package com.spring_boot_project.journalApp.service;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+
+@Slf4j
 public class EmailService {
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    public void sendEmail(String to , String subject , String body){
+        try {
+            SimpleMailMessage mail = new SimpleMailMessage();
+            mail.setTo(to);
+            mail.setSubject(subject);
+            mail.setText(body);
+
+            javaMailSender.send(mail);
+            log.info("Email sent successfully to {}", to);
+        } catch (Exception e) {
+            log.error("Exception while sending mail", e);
+        }
+    }
 }
